1e7ba8c2d50733acf382bd305e95a12fb871f981
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-13 19:10:34 -0300

    chore: initializing jest configuration

---
+++ api/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+};
