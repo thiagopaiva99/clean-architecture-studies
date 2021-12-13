e346b2d9a94d889cee94d7180d01fc1265b77dff
Thiago Galvani <thiagopaiva99@gmail.com>
2021-12-13 20:25:11 -0300

    test: add and find user, and check if user exists on repository

--- api/src/usecases/repository/in-memory-user-repository.spec.ts
+++ api/src/usecases/repository/in-memory-user-repository.spec.ts
@@ -1,4 +1,4 @@
-import { internet } from 'faker'
+import { internet, name } from 'faker'
 import { UserRepository } from '../ports/user-repository.model'
 import { UserData } from '../register-user-on-mail-list/user-data.model'
 import { InMemoryUserRepository } from './in-memory-user-repository'
@@ -9,5 +9,15 @@ describe('InMemoryUserRepository', () => {
     const userRepository: UserRepository = new InMemoryUserRepository(users)
     const user = await userRepository.findByEmail(internet.email())
     expect(user).toBeNull()
+  })
+
+  test('should return user if it is found in the repository', async () => {
+    const users: UserData[] = []
+    const userName = name.findName()
+    const email = internet.email()
+    const userRepository = new InMemoryUserRepository(users)
+    await userRepository.add({ name: userName, email })
+    const user = await userRepository.findByEmail(email)
+    expect(user.name).toBe(userName)
   })
 })
